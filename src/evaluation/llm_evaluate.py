e6283952dacf6e101822e62ff391048df3c64ecc
hngocngvu <hongngocnguyenvu.work@gmail.com>
2026-03-21 01:18:40 +0700

    Done evaluation for MedQA & MedMCQA using qwen2.5-3b-instruct

--- src/evaluation/llm_evaluate.py
+++ src/evaluation/llm_evaluate.py
@@ -5,6 +5,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 import os
 from pathlib import Path
 import sys
+import pandas as pd 
 
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -12,14 +13,16 @@ if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
 
 from src.llm.llm import QwenMCQ
-from src.llm.utils import build_medmcqa_prompt
+from src.llm.utils import build_medmcqa_prompt, build_medqa_prompt
 
 MEDMCQA_PATH = os.path.join(ROOT_DIR, "data", "processed", "medmcqa", "medmcqa.json")
-OUTPUT_PATH = os.path.join(ROOT_DIR, "src", "evaluation", "llm_eval.json")
+MEDQA_PATH = os.path.join(ROOT_DIR, "data", "external", "medqa", "medqa_form2.csv")
+OUTPUT_PATH_MEDMCQA = os.path.join(ROOT_DIR, "src", "evaluation", "llm_eval_medmcqa.json")
+OUTPUT_PATH_MEDQA = os.path.join(ROOT_DIR, "src", "evaluation", "llm_eval_medqa.json")
 
 device = "cuda"
 
-PROMPT_TEMPLATE = """
+PROMPT_TEMPLATE_MEDMCQA = """
 You are answering a medical multiple choice question.
 
 Choose the correct answer from A, B, C, or D.
@@ -29,6 +32,24 @@ Do not explain.
 
 Question:
 {question}
+
+Answer:
+"""
+
+PROMPT_TEMPLATE_MEDQA = """
+You are answering a medical multiple choice question.
+
+Select the single best answer.
+
+Return ONLY ONE LETTER (A, B, C, D, or E).
+Do not explain your reasoning.
+IF YOUR ANSWER IS UNKNOWN, CHANGE IT TO E
+
+Question:
+{question}
+
+Options:
+{options}
 
 Answer:
 """
@@ -43,17 +64,47 @@ if __name__ == "__main__":
     with open(MEDMCQA_PATH) as f:
         data = json.load(f)
 
+
+    df= pd.read_csv(MEDQA_PATH)
+    data1 = df.to_dict(orient="records")
+
     # random 1000 samples with seed
     random.seed(42)
     data_medmcqa = random.sample(data, min(1000, len(data)))
+    data_medqa= random.sample(data1, min(1000, len(data1)))
 
     preds_medmcqa = []
     gts_medmcqa = []
     results_medmcqa = []
 
-    for item in tqdm(data_medmcqa):
 
-        prompt = build_medmcqa_prompt(item, PROMPT_TEMPLATE)
+    preds_medqa = []
+    gts_medqa = []
+    results_medqa = []
+
+
+
+    for item in tqdm(data_medqa, desc="Evaluating MedQA"):
+
+        prompt = build_medqa_prompt(item, PROMPT_TEMPLATE_MEDQA)
+
+        pred = llm.predict(prompt)
+        if pred == "UNKNOWN":
+            pred= "E"
+        gt = item["answer"]
+
+        preds_medqa.append(pred)
+        gts_medqa.append(gt)
+
+        results_medqa.append({
+            "question": item["question"],
+            "prediction": pred,
+            "groundtruth": gt
+        })
+
+    for item in tqdm(data_medmcqa, desc= "Evaluating MedMCQA"):
+
+        prompt = build_medmcqa_prompt(item, PROMPT_TEMPLATE_MEDMCQA)
 
         pred = llm.predict(prompt)
         gt = item["answer"]
@@ -67,6 +118,8 @@ if __name__ == "__main__":
             "groundtruth": gt
         })
 
+
+    
     # metrics
     accuracy_medmcqa = accuracy_score(gts_medmcqa, preds_medmcqa)
 
@@ -86,12 +139,36 @@ if __name__ == "__main__":
         zero_division=0
     )
 
+
+    accuracy_medqa = accuracy_score(gts_medqa, preds_medqa)
+
+    precision_medqa = precision_score(
+        gts_medqa,
+        preds_medqa,
+        labels=["A", "B", "C", "D", "E"],
+        average="macro",
+        zero_division=0
+    )
+
+    recall_medqa = recall_score(
+        gts_medqa,
+        preds_medqa,
+        labels=["A", "B", "C", "D", "E"],
+        average="macro",
+        zero_division=0
+    )
+
+
     print("Accuracy (MedMCQA):", accuracy_medmcqa)
     print("Precision (MedMCQA):", precision_medmcqa)
     print("Recall (MedMCQA):", recall_medmcqa)
 
+    print("Accuracy (MedQA):", accuracy_medqa)
+    print("Precision (MedQA):", precision_medqa)
+    print("Recall (MedQA):", recall_medqa)
+
     # save results
-    with open(OUTPUT_PATH, "w") as f:
+    with open(OUTPUT_PATH_MEDMCQA, "w") as f:
         json.dump({
             "metrics": {
                 "accuracy": accuracy_medmcqa,
@@ -99,4 +176,15 @@ if __name__ == "__main__":
                 "recall": recall_medmcqa
             },
             "predictions": results_medmcqa
+        }, f, indent=2)
+
+    # save results
+    with open(OUTPUT_PATH_MEDQA, "w") as f:
+        json.dump({
+            "metrics": {
+                "accuracy": accuracy_medqa,
+                "precision": precision_medqa,
+                "recall": recall_medqa
+            },
+            "predictions": results_medqa
         }, f, indent=2)
